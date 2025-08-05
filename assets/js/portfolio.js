243f36785d424d86b12519a7d24fd3f72d426907
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-08-05 16:26:31 +0530

    first commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,9 @@
+// Example: Smooth scroll or other interaction
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
